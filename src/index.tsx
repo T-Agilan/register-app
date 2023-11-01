6aa97bf0acc86e63d1f62bf5f4d138ea38b025b6
T-Agilan <agilanakil1432@gmail.com>
2023-11-01 17:03:12 +0530

    update app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Multiple from './App';
 import reportWebVitals from './reportWebVitals';
+// import Multiple from './multiple';
+import App from './App';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Multiple />
+  {/* <Multiple/> */}
+  <App/>
   </React.StrictMode>
 );
 
