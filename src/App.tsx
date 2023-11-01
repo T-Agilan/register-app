6adda76b797779775b3104cfee6e6f4c8f000210
T-Agilan <agilanakil1432@gmail.com>
2023-11-01 17:04:53 +0530

    set axios package ,display outputdata in db

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import "./App.css";
 import React, { useState, ChangeEvent, FormEvent,useEffect } from "react";
-import OutputTable, { inputData } from "./outputtable";
+// import OutputTable, { inputData } from "./outputtable";
 const initialFormData: inputData = {
   fname: "",
   mail: "",
@@ -27,7 +28,8 @@ const Multiple: React.FC = () => {
     null
   );
   
-  localStorage.setItem('dataKey', JSON.stringify(data));
+  localStorage.setItem('submittedData', JSON.stringify(submittedData));
+
   const [filteredData, setFilteredData] = useState<inputData[]>(submittedData);
   useEffect(() => {
     const storedSubmittedData = localStorage.getItem('submittedData');
@@ -44,21 +46,26 @@ const Multiple: React.FC = () => {
     setformData({ ...formData, [name]: value });
   };
   
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    console.log("hii")
     event.preventDefault();
-    if (editingIndex === null) {
-      const updatedSubmittedData = [...submittedData, formData];
-      setSubmittedData(updatedSubmittedData);
-      localStorage.setItem('submittedData', JSON.stringify(updatedSubmittedData));
-    } else {
-      const updatedDataList = [...submittedData];
-      updatedDataList[editingIndex] = formData;
-      setSubmittedData(updatedDataList);
-      localStorage.setItem('submittedData', JSON.stringify(updatedDataList));
-      setEditingIndex(null);
+    
+    const updatedData = [...submittedData, formData];
+    setSubmittedData(updatedData);
+
+    // Save to local storage
+    localStorage.setItem('submittedData', JSON.stringify(updatedData));
+
+    setformData(initialFormData);
+    try {
+      const response = await axios.post("http://localhost:3002/", formData);
+      console.log(response.data, "success");
+    } catch (error) {
+      console.error(error, "error");
     }
     setformData(initialFormData);
   };
+  
   const handleEdit = (data: inputData, index: number) => {
     setformData(data);
     setEditingIndex(index);
@@ -94,7 +101,7 @@ const Multiple: React.FC = () => {
   return (
     <>
       <div>
-        <h2>Merchant Form</h2>
+        <h2>Merchant Fgvfhgorm</h2>
         <form onSubmit={handleSubmit} id="form">
           <table className="table1">
             <tr>
@@ -493,3 +500,83 @@ const Multiple: React.FC = () => {
   );
 };
 export default Multiple;
+
+export interface inputData {
+  fname: string;
+  mail: string;
+  number: string;
+  website: string;
+  contactName: string;
+  contactPhone: string;
+  contactMail: string;
+  notes: string;
+  type: string;
+  category: string;
+  percentage: number;
+  activeFrom: string;
+  // Logo:image;
+  criticalAccount: string;
+  paymentOptions: string;
+}
+interface PropsType {
+  submittedData: inputData[];
+  setSubmittedData: React.Dispatch<React.SetStateAction<inputData[]>>;
+  onEdit: (data: inputData, index: number) => void;
+  onDelete: (index: number) => void;
+}
+
+const OutputTable: React.FC<PropsType> = ({
+  submittedData,
+  // setSubmittedData,
+  onEdit,
+  onDelete,
+}) => {
+  return (
+    <table id="dataTable" className="table-scroll">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Website</th>
+          <th>Contact Name</th>
+          <th>Contact Phone</th>
+          <th>Contact Email</th>
+          <th>Notes</th>
+          <th>Type</th>
+          <th>Category</th>
+          <th>Commission Percentage</th>
+          <th>Active From</th>
+          {/* <th>Logo</th> */}
+          <th>Critical Account</th>
+          <th>Payment Options</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {submittedData.map((data, index) => (
+          <tr key={index}>
+            <td>{data.fname}</td>
+            <td>{data.mail}</td>
+            <td>{data.number}</td>
+            <td>{data.website}</td>
+            <td>{data.contactName}</td>
+            <td>{data.contactPhone}</td>
+            <td>{data.contactMail}</td>
+            <td>{data.notes}</td>
+            <td>{data.type}</td>
+            <td>{data.category}</td>
+            <td>{data.percentage}</td>
+            <td>{data.activeFrom}</td>
+            <td>{data.criticalAccount}</td>
+            <td>{data.paymentOptions}</td>
+            <td>
+              <button onClick={() => onEdit(data, index)}>Edit</button>
+              <button onClick={() => onDelete(index)}>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
